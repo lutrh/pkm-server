99bbcb748ea3f1e3f51bb16a0fe1889699b507bd
Luthfi Hidayattullah <luthfirahmad24@gmail.com>
2017-10-22 11:14:42 +0700

    add sample data

--- classify_image/dictionary.py
+++ classify_image/dictionary.py
@@ -9,7 +9,7 @@ from django.shortcuts import render
 @csrf_exempt
 def dictionary_api(request):
     data = []
-    data.append({"nama": "belalang", "nama_latin": 2, "deskripsi": 3, "solusi": 4, "ditemukan": 5, "suhu_hidup": 6})
+    data.append({"nama": "belalang", "nama_latin": "Caelifera", "deskripsi": "Belalang adalah serangga herbivora dari subordo Caelifera dalam ordo Orthoptera", "solusi": "Semprot tanaman dengan pestisida organik", "ditemukan": "leaves", "suhu_hidup": "25"})
     data.append({"nama": "walang sangit", "nama_latin": 2, "deskripsi": 3, "solusi": 4, "ditemukan": 5, "suhu_hidup": 6})
     data.append({"nama": "tikus sawah", "nama_latin": 2, "deskripsi": 3, "solusi": 4, "ditemukan": 5, "suhu_hidup": 6})
     data.append({"nama": "wereng", "nama_latin": 2, "deskripsi": 3, "solusi": 4, "ditemukan": 5, "suhu_hidup": 6})
